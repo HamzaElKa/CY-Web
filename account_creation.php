014f87af8feeff08a13e265997147371c4a00bb5
HamzaElKa <130193862+HamzaElKa@users.noreply.github.com>
2024-05-02 01:43:13 +0200

    First PHP code

---
+++ account_creation.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(isset($_POST['ok'])) {
+        var_dump($_POST);
+        $firstname = $_POST["firstname"];
+        $name = $_POST["name"];
+        $birthdate = $_POST["birthdate"];
+        $gender = $_POST["gender"];
+        $physical_description = isset($_POST["physical_description"]) ? $_POST["physical_description"] : "";
+        $relationship_status = isset($_POST["relationship_status"]) ? $_POST["relationship_status"] : "";
+        $city = $_POST["city"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $passwordConf = $_POST["passwordConf"];
+            $filename = 'utilisateurs.txt';
+            $data = $firstname . ',' .$name . ',' . $birthdate . ',' . $gender . ',' . $physical_description .',' .$relationship_status . ',' .$city .',' .$email . ',' .$password .',' . PHP_EOL;
+            file_put_contents($filename, $data, FILE_APPEND | LOCK_EX);            
+            header("Location: page_connexion.html");     
+      }  } 
+ ?>
